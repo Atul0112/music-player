609ede3f8d990c57fe633dec37a51eb42c673eaf
Atul Kumar <129223263+Atul0112@users.noreply.github.com>
2023-09-25 11:49:01 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere - Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Justin Bieber - Baby (Official Music) ft. Ludacris",
+    artist: "Justin Bieber",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bad Blood",
+    artist: "Taylor Swift",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+];
